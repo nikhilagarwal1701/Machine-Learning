51381b9427272e9fe49730750d20b8802c4937d5
Nikhil Agarwal <51421036+nikhilagarwal1701@users.noreply.github.com>
2020-05-25 02:19:41 +0530

    Update Rstudio_code.R

--- 4. Polynomial Linear Regression/Rstudio_code.R
+++ 4. Polynomial Linear Regression/Rstudio_code.R
@@ -4,18 +4,14 @@
 dataset=read.csv('Position_Salaries.csv')
 dataset=dataset[2:3]
 
-# Fitting Linear Regression to the dataset
-lin_reg=lm(formula=Salary~.,
-           data=dataset)
-
-# Fitting Linear Regression to the dataset
+# Fitting Polynomial Linear Regression to the dataset
 dataset$Level2=dataset$Level^2
 dataset$Level3=dataset$Level^3
 dataset$Level4=dataset$Level^4
 poly_reg=lm(formula=Salary~.,
             data=dataset)
 
-# Visualising the Linear Regression results
+# Visualising Polynomial Linear Regression results
 x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
@@ -30,7 +26,7 @@ ggplot() +
   xlab('Level') +
   ylab('Salary')
 
-# Predicting a new result with Polynomial Regression
+# Predicting a new result with Polynomial Linear Regression
 predict(poly_reg, data.frame(Level = 6.5,
                              Level2 = 6.5^2,
                              Level3 = 6.5^3,
