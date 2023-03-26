c3ea6557ae17e6d26a35d4fa34d1a130ba7709d1
Nikhil Agarwal <nagarwal1701@gmail.com>
2023-03-26 19:53:55 +0530

    Missing Data in R

---
+++ 2023 Starting Fresh/1. Data Preprocessing/data_preprocessing_template.R
@@ -0,0 +1,10 @@
+dataset = read.csv('Data.csv')
+
+#Missing Data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
