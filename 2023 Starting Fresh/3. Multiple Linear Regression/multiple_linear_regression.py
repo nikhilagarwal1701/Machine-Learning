51fe813574daceb9d9d932bf7381d2132e5fda7b
Nikhil Agarwal <nagarwal1701@gmail.com>
2023-04-15 21:44:50 +0530

    single and multiple linear (with backward elimination) regression done

---
+++ 2023 Starting Fresh/3. Multiple Linear Regression/multiple_linear_regression.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+import statsmodels.api as sm
+
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Encoding the 'States' column
+labelEncoder_X = LabelEncoder()
+X[:, 3] = labelEncoder_X.fit_transform(X[:, 3])
+columnTransformer = ColumnTransformer([("State", OneHotEncoder(), [3])], remainder='passthrough')
+X = columnTransformer.fit_transform(X)
+
+# Avoiding the Dummy Variable Trap
+X = X[:, 1:]
+
+# Training and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Feature Scaling
+# sc_X = StandardScaler()
+# X_train = sc_X.fit_transform(X_train)
+# X_test = sc_X.transform(X_test)
+
+# Fitting Multiple Linear Regression to the Training set
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+# Building the optimal model using Backward Elimination
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+X = np.array(X, dtype=float)
+# X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+# regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+#
+# print(regressor_OLS.summary())
+
+X_opt = X[:, [0, 3]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+
+print(regressor_OLS.summary())
